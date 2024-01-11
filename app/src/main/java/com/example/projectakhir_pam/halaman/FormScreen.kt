a566c8739bbe0884de5d6b53e1d2cb4b80a10b40
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2024-01-11 17:50:17 +0700

    menambahkan function FormScreen

--- app/src/main/java/com/example/projectakhir_pam/halaman/FormScreen.kt
+++ app/src/main/java/com/example/projectakhir_pam/halaman/FormScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -13,9 +15,13 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -23,10 +29,56 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.projectakhir_pam.R
 import com.example.projectakhir_pam.model.DetailPemesan
+import com.example.projectakhir_pam.model.EntryViewModel
 import com.example.projectakhir_pam.model.UIStatePemesan
+import com.example.projectakhir_pam.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
 
+object FormDestination : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormScreen(
+    navigateNext:() -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    entryViewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory),
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            HotelTopAppBar(
+                title = stringResource(FormDestination.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            uiStatePemesan = entryViewModel.uiStatePemesan,
+            onPemesanValueChange = entryViewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    entryViewModel.savePemesan()
+                    navigateNext()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 @Composable
 fun EntryBody(
     uiStatePemesan: UIStatePemesan,
